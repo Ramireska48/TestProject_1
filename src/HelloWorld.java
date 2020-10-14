51d30b07ff0b161810b3900c147df79f0c46bc16
Ramireska48 <Ramires.Elena48@gmail.com>
2020-10-15 00:14:42 +0300

    Домашка за 1 урок

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         //sout
         System.out.println("Hello world");
+        System.out.println("Update!");
     }
 
 }
